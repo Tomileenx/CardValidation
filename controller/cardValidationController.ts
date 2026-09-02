6400e219b25185dfc89428d5a1acdf62f9fc6dcd
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-02 11:44:44 +0100

    feat: implement card validation controller

---
+++ controller/cardValidationController.ts
@@ -0,0 +1,44 @@
+import type { Request, Response } from "express";
+import type { CardValidationRequest } from "../interfaces/CardValidationRequest.js";
+import type { CardValidationResponse } from "../interfaces/CardValidationResponse.js";
+import { validateCardNumber } from "../service/cardValidationService.js";
+
+export function validateCard(
+    req: Request<{}, {}, CardValidationRequest>,
+    res: Response
+): void {
+    const { cardNumber } = req.body;
+
+    // Check if card number are inputed
+    if (!cardNumber) {
+        const response: CardValidationResponse = {
+            isNumberValid: false,
+            message: "Card number is required"
+        }
+
+        res.status(400).json(response);
+        return;
+    }
+
+    // check if card number input is a string
+    if (typeof cardNumber !== "string") {
+        const response: CardValidationResponse = {
+            isNumberValid: false,
+            message: "Card number must be a string"
+        }
+
+        res.status(400).json(response);
+        return;
+    }
+
+    const isCardNumberValid = validateCardNumber(cardNumber);
+
+    const response: CardValidationResponse = {
+        isNumberValid: true,
+        message: isCardNumberValid
+            ? "Card number is valid"
+            : "Card number is not valid"
+    }
+
+    res.status(200).json(response);
+}
