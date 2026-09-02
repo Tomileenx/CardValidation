03cee4d50c996ddd05914cccbbe373815483eec4
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-02 18:58:51 +0100

    feat: add changes to card validation controller

--- controller/cardValidationController.ts
+++ controller/cardValidationController.ts
@@ -9,7 +9,6 @@ export function validateCard(
 ): void {
     const { cardNumber } = req.body;
 
-    // Check if the card number is provided
     if (!cardNumber) {
         const response: CardValidationResponse = {
             isNumberValid: false,
@@ -20,7 +19,6 @@ export function validateCard(
         return;
     }
 
-    // check if the card number input is a string
     if (typeof cardNumber !== "string") {
         const response: CardValidationResponse = {
             isNumberValid: false,
