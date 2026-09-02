98e31e7d237690f2a2ac82b93b66ff3fb1e4d827
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-02 19:01:01 +0100

    feat: add changes to index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
-// import express
 import express from 'express';
 import router from './route/cardValidationRoutes.js';
 
-// create express app
 const app = express();
 
-// middleware accepting JSON
 app.use(express.json());
 
-// mount router
 app.use("/api/", router);
 
-// port number
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
