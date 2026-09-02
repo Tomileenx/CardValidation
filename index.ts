32fc0f49aaed4bcb7f0dbac4b8b8094fb28b4ebd
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-02 14:41:58 +0100

    feat: implement index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,19 @@
 // import express
 import express from 'express';
+import router from './route/cardValidationRoutes.js';
 
 // create express app
 const app = express();
+
+// middleware accepting JSON
+app.use(express.json());
+
+// mount router
+app.use("/api/", router);
+
+// port number
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
