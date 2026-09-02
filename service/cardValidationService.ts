f884c3b20c5bf0a31933b2f3ea17fc0d4340d302
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-02 10:58:15 +0100

    feat: implement card validation service

---
+++ service/cardValidationService.ts
@@ -0,0 +1,46 @@
+export function validateCardNumber(cardNumber: string): boolean {
+    // removes all spaces and hyphens from the card number
+    const digits = cardNumber.replace(/[\s-]/g, "");
+
+    // check that the card number contains only digits 
+    const isAllDigits = /^\d+$/.test(digits);
+    
+    // return false if card number contains contain non-digit characters
+    if (!isAllDigits) {
+        return false
+    }
+
+    // return false if card numbers is not up to 16
+    if (digits.length !== 16) {
+        return false
+    }
+
+    // validate the card number using luhn algorithm
+    let sum = 0;
+    let shouldDoubleDigit = false;
+
+    // loop through the digits from the last index to first index 
+    for (let i = digits.length -  1; i >= 0; i--) {
+        // convert the string digit to number
+        let numberDigit = Number(digits[i]);
+
+        if (shouldDoubleDigit) {
+            // if shouldDoubleDigit is true double numberDigit by 2
+            numberDigit *= 2
+
+            // if result of numberDigit is greater than 9, then subtract numberDigit by 9
+            if (numberDigit > 9) {
+                numberDigit -= 9;
+            }
+        }
+        
+        // add the numberDigit to sum
+        sum += numberDigit;
+
+        // alternate whether next digit should be doubled
+        shouldDoubleDigit = !shouldDoubleDigit
+    }
+
+    // checks if sum is divisible by 10
+    return sum % 10 === 0;
+}
