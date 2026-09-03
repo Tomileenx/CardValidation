27be60a9b4b69d737f150c6a75cb53edab890792
Tomileenx <olarewajuoluwatomi@gmail.com>
2026-09-03 08:16:25 +0100

    feat: add changes to card validation route

--- route/cardValidationRoutes.ts
+++ route/cardValidationRoutes.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import { validateCard } from '../controller/cardValidationController.js';
 
-// create express router
 const router = express.Router();
 
-// create POST endpoint
 router.post('/validateCardNumber', validateCard);
 
 export default router;
